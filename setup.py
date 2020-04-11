ab75b0a57043aedee58adf601f4ae1f6d1975c17
cytopia <cytopia@everythingcli.org>
2020-04-11 13:14:03 +0200

    Update pip classifier

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+"""Pip configuration."""
 from setuptools import setup
 
 with open("README.md", "r") as fh:
@@ -13,28 +14,38 @@ setup(
     author="cytopia",
     author_email="cytopia@everythingcli.org",
     url="https://github.com/cytopia/badchars",
-    install_requires=["argparse"],
+    install_requires=[],
     scripts=[
         "badchars"
     ],
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
+        # https://pypi.org/classifiers/
+        #
+        # How mature is this project
         'Development Status :: 5 - Production/Stable',
-
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        # Project topics
+        "Topic :: Communications :: Chat",
+        "Topic :: Communications :: File Sharing",
+        "Topic :: Internet",
+        "Topic :: Security",
+        "Topic :: System :: Shells",
+        "Topic :: System :: Systems Administration",
+        "Topic :: Utilities",
         # License
         "License :: OSI Approved :: MIT License",
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
+        # How does it run
+        "Environment :: Console",
+        # Where does it rnu
         "Operating System :: OS Independent",
     ],
  )
